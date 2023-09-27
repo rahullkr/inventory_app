0bd76695171882878b070eb9b71370d58282fcbd
rahullkr <rk7318323176@gmail.com>
2023-09-28 05:03:48 +0530

    made models for backend

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,21 +1,43 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please add a name"],
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add a email"],
+      unique: true,
+      trim: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please fill a valid email address",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+      minLength: [3, "Password must be more than 8 characters"],
+    },
+    photo: {
+      type: String,
+      required: [true, "Please add a photo"],
+      default: "https://picsum.photos/200/300",
+    },
+    phone: {
+      type: String,
+      default: "+91",
+    },
+    bio: {
+      type: String,
+      maxLength: [100, "Bio should not be more than 100 characters"],
+      default: "bio",
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Please add a email"],
-    unique: true,
-    trim: true,
-    match: [
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      "Please fill a valid email address",
-    ],
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
