a19e13b170752a7c31fc6e5a29a3fb2ce52212f2
rahullkr <rk7318323176@gmail.com>
2023-09-28 07:03:14 +0530

    drawbacks removed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,7 +10,7 @@ const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const { Error } = require("mongoose");
 
-const bcrypt = require("bcryptjs");
+
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
@@ -34,12 +34,11 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   // encryption, user creation ke pehle hoga
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
+
   const user = await User.create({
     name,
     email,
-    password: hashedPassword,
+    password
   });
   if (user) {
     const { _id, name, email, photo, phone, bio } = user;
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema(
   {
@@ -38,6 +39,18 @@ const userSchema = mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+
+  if(!this.isModified("password")){
+       return next()
+  }
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(this.password, salt);
+
+  this.password = hashedPassword;
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
