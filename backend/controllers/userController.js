32f14a6ec93d1f47525070b330e41e463653ff22
rahullkr <rk7318323176@gmail.com>
2023-09-28 05:30:48 +0530

    added some middlewares

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,8 @@
-const registerUser = async(req,res) => {
+const registerUser = (req,res) => {
+    if(!req.body.email){
+        res.status(400);
+            throw new Error('Please enter the email');
+    }
     res.send('register user');
 }
 
--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,8 @@ const bodyParser = require("body-parser");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 const mongoose = require("mongoose");
-const userRoute = require('./routes/userRoutes')
-
+const userRoute = require("./routes/userRoutes");
+const errorHandler = require("./middlewares/userMiddlewares");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -12,13 +12,11 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-
 app.get("/", (req, res) => {
   res.send("home");
 });
-app.use('/api/users', userRoute);
-
+app.use("/api/users", userRoute);
+app.use(errorHandler);
 
 mongoose
   .connect(process.env.MONGO_URI)
