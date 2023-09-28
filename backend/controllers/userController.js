c07b8e8ee88b740250d7b856a844b6557854637a
rahullkr <rk7318323176@gmail.com>
2023-09-28 16:14:22 +0530

    created login routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,11 +9,13 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const { Error } = require("mongoose");
-const jwt = require('jsonwebtoken')
-const generateToken = (id) => {
-    return jwt.sign({id}, process.env.JWT_SECRETS, {expiresIn: '1d'})
-}
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRETS, { expiresIn: "1d" });
+};
+// register user
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
@@ -36,31 +38,24 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("email already exists");
   }
 
-
-
   const user = await User.create({
     name,
     email,
-    password
+    password,
   });
 
-    // generate token
-    const token = generateToken(user._id)
+  // generate token
+  const token = generateToken(user._id);
 
-    res.cookie('token', token, {
-        path: '/',
-        httpOnly: true,
-        expires: new Date(Date.now() + 1000*86400),
-        sameSite: "none",
-        secure: true
-    })
+  res.cookie("token", token, {
+    path: "/",
+    httpOnly: true,
+    expires: new Date(Date.now() + 1000 * 86400),
+    sameSite: "none",
+    secure: true,
+  });
 
-
-
-
-
-
-     if (user) {
+  if (user) {
     const { _id, name, email, photo, phone, bio } = user;
     res.status(201).json({
       _id,
@@ -77,4 +72,56 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = registerUser;
+// login user
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("invalid user data");
+  }
+
+  // now the checking part
+  const user = await User.findOne({ email });
+  if (!user) {
+    res.status(400);
+    throw new Error("user not found, sign up right now");
+ 
+  }
+  const token = generateToken(user._id);
+
+  res.cookie("token", token, {
+    path: "/",
+    httpOnly: true,
+    expires: new Date(Date.now() + 1000 * 86400),
+    sameSite: "none",
+    secure: true,
+  });
+
+  const passwordIsCorrect = await bcrypt.compare(password, user.password);
+  if (user && passwordIsCorrect) {
+    const { _id, name, email, photo, phone, bio } = user;
+    res.status(200).json({
+      _id,
+      name,
+      email,
+      photo,
+      phone,
+      bio,
+      token,
+    });
+  }
+  else{
+    res.status(400);
+    throw new Error("invalid email or password");
+  }
+  
+});
+
+
+// logout user
+
+const logout = asyncHandler(async (req,res) =>{
+
+})
+
+module.exports = { registerUser, loginUser, logout };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const registerUser = require("../controllers/userController");
+const {registerUser, loginUser, logout} = require("../controllers/userController");
 router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/logout", logout);
 module.exports = router;
