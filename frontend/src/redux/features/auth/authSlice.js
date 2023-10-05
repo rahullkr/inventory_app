39d59b97c6282bccfafd8d5a83d3934a622fe6c7
rahullkr <rk7318323176@gmail.com>
2023-10-05 22:02:06 +0530

    added frontend and made files in frontend with routes

---
+++ frontend/src/redux/features/auth/authSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+
+}
+
+const authSlice = createSlice({
+  name: "atuh",
+  initialState,
+  reducers: {}
+});
+
+export const {} = authSlice.actions
+
+export default authSlice.reducer
